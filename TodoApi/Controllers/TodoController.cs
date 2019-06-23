51a6112a8de40a2a1d1344eb4c0343d8a185d8f8
Krisz <kriszrokakaszas@gmail.com>
2019-06-23 18:25:31 +0200

    Startup.cs amended, the app works now.

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -91,24 +91,5 @@ namespace TodoApi.Controllers
 
             return NoContent();
         }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for 
-                // production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseHttpsRedirection();
-            app.UseMvc();
-        }
     }
 }
